79051f1a210e36abda2643217e1ab499cb16d9f5
Ataraxist <cantico@gmail.com>
2025-09-06 04:26:35 -0700

    Removed Legacy Version Code

--- legacy-v1/Server/routes/dataRouter.js
+++
@@ -1,8 +0,0 @@
-// import express from "express";
-// import dataController from "../controllers/dataController.js";
-
-// export const router = express.Router();
-
-// router.get("/getData", dataController.getDataFromDatabase);
-
-// export default router
